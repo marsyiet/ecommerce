15d7c0386aea7bfc3122409ce2466a79dad9c5db
marius <etoundmarius237@gmail.com>
2023-01-15 09:15:21 +0100

    bd

--- ecommerce (1).sql
+++ ecommerce (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 27 déc. 2022 à 10:45
+-- Généré le : dim. 15 jan. 2023 à 08:14
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `administrateurs` (
   `password` varchar(255) DEFAULT NULL,
   `etat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `administrateurs`
@@ -42,7 +42,30 @@ CREATE TABLE IF NOT EXISTS `administrateurs` (
 
 INSERT INTO `administrateurs` (`id`, `login`, `password`, `etat`) VALUES
 (1, 'marsyiet', 'ijokpl', 0),
-(2, 'kiki', 'ijokpl', 0);
+(2, 'kiki', 'ijokpl', 0),
+(3, 'toto', 'ijokpl', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `baner`
+--
+
+DROP TABLE IF EXISTS `baner`;
+CREATE TABLE IF NOT EXISTS `baner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `alaune` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `baner`
+--
+
+INSERT INTO `baner` (`id`, `image`, `nom`, `alaune`) VALUES
+(1, 'banner.jpg', 'legumes', 1);
 
 -- --------------------------------------------------------
 
@@ -81,19 +104,19 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `ville` int(11) NOT NULL,
   `quartier` int(11) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `etat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `ville` (`ville`),
   KEY `quartier` (`quartier`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `clients`
 --
 
-INSERT INTO `clients` (`id`, `image`, `nom`, `mail`, `ville`, `quartier`, `password`, `etat`) VALUES
-(2, 'mbappe.jpg', 'mbappe', 'mbappe@gmail.com', 1, 1, 'ijokpl', 0),
-(3, 'dem.jpg', 'dembouz', 'dembouz@dembouz.com', 3, 4, 'ijokpl', 0);
+INSERT INTO `clients` (`id`, `image`, `nom`, `mail`, `ville`, `quartier`, `password`) VALUES
+(2, 'mbappe.jpg', 'mbappe', 'mbappe@gmail.com', 1, 1, 'ijokpl'),
+(3, 'dem.jpg', 'dembouz', 'dembouz@dembouz.com', 3, 4, 'ijokpl'),
+(4, 'trent.jpg', 'trent', 'trent@gmail.com', 1, 6, 'ijokpl');
 
 -- --------------------------------------------------------
 
@@ -129,7 +152,16 @@ CREATE TABLE IF NOT EXISTS `dislikes` (
   `id_produit` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=112 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `dislikes`
+--
+
+INSERT INTO `dislikes` (`id`, `id_produit`, `id_client`) VALUES
+(92, 9, 2),
+(73, 7, 2),
+(90, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -148,14 +180,15 @@ CREATE TABLE IF NOT EXISTS `fournisseurs` (
   PRIMARY KEY (`id`),
   KEY `ville` (`ville`),
   KEY `quartier` (`quartier`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `fournisseurs`
 --
 
 INSERT INTO `fournisseurs` (`id`, `nom`, `mail`, `ville`, `quartier`, `etat`) VALUES
-(1, 'marius', 'etoundimarius237@gmail.com', 3, 1, 0);
+(1, 'marius', 'etoundimarius237@gmail.com', 3, 1, 0),
+(2, 'yoahn', 'yoahn@gmail.com', 3, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -169,19 +202,17 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `id_produit` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=189 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `likes`
 --
 
 INSERT INTO `likes` (`id`, `id_produit`, `id_client`) VALUES
-(28, 9, 2),
-(27, 3, 2),
-(26, 6, 2),
-(25, 8, 2),
-(23, 7, 3),
-(20, 7, 2);
+(188, 4, 2),
+(157, 3, 2),
+(168, 2, 2),
+(135, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -200,14 +231,28 @@ CREATE TABLE IF NOT EXISTS `livreurs` (
   PRIMARY KEY (`id`),
   KEY `ville` (`ville`),
   KEY `quartier` (`quartier`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `livreurs`
 --
 
 INSERT INTO `livreurs` (`id`, `nom`, `mail`, `ville`, `quartier`, `etat`) VALUES
-(1, 'marius', 'etoundimarius237@gmail.com', 3, 1, 0);
+(1, 'marius', 'etoundimarius237@gmail.com', 3, 1, 0),
+(3, 'yoahn', 'yoahn@gmail.com', 3, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `online`
+--
+
+DROP TABLE IF EXISTS `online`;
+CREATE TABLE IF NOT EXISTS `online` (
+  `id` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  `ipuser` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -221,7 +266,7 @@ CREATE TABLE IF NOT EXISTS `paiements` (
   `libelle` varchar(255) DEFAULT NULL,
   `etat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `paiements`
@@ -230,7 +275,10 @@ CREATE TABLE IF NOT EXISTS `paiements` (
 INSERT INTO `paiements` (`id`, `libelle`, `etat`) VALUES
 (1, 'orange money', 0),
 (2, 'mtn mobile money', 0),
-(3, 'express-union', 0);
+(3, 'express-union', 0),
+(10, 'moneygram', 0),
+(7, 'paypal', 0),
+(8, 'express-exchange', 0);
 
 -- --------------------------------------------------------
 
@@ -248,28 +296,26 @@ CREATE TABLE IF NOT EXISTS `produits` (
   `fournisseur` int(11) NOT NULL,
   `date` date NOT NULL,
   `qte` int(11) NOT NULL,
-  `alaune` tinyint(1) NOT NULL DEFAULT '0',
-  `etat` tinyint(1) NOT NULL DEFAULT '0',
-  `favori` tinyint(1) NOT NULL DEFAULT '0',
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cathegorie` (`cathegorie`),
   KEY `fournisseur` (`fournisseur`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `produits`
 --
 
-INSERT INTO `produits` (`id`, `image`, `nom`, `prix`, `cathegorie`, `fournisseur`, `date`, `qte`, `alaune`, `etat`, `favori`, `description`) VALUES
-(2, 'jusOrange.jpg', 'jus d\'orange', 0, 1, 1, '2022-05-12', 30, 0, 0, 1, 'jus d\'orange bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala bla bla bla bla bala \r\n'),
-(3, 'raisins.jpg', 'raisins', 0, 2, 1, '2022-11-22', 10, 0, 0, 0, ''),
-(9, 'pomme.jpg', 'pommes', 3000, 2, 1, '2022-12-10', 50, 0, 0, 0, ''),
-(4, 'jusRaisin.jpg', 'jus de raisins', 1500, 1, 1, '2022-11-28', 40, 0, 0, 0, ''),
-(6, 'mangue.jpg', 'Mangue', 3000, 2, 1, '2022-11-12', 10, 0, 0, 0, ''),
-(7, 'orange.jpg', 'Oranges', 3000, 2, 1, '2022-11-28', 50, 0, 0, 1, 'oranges blabla blablablablablablablablablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla lablablablabla \r\n'),
-(8, 'pasteque.jpg', 'Pasteque', 3000, 2, 1, '2022-02-13', 30, 1, 0, 0, ''),
-(10, 'briqueJus.jpg', 'jus cocktail', 4000, 1, 1, '2022-12-14', 45, 0, 0, 0, '');
+INSERT INTO `produits` (`id`, `image`, `nom`, `prix`, `cathegorie`, `fournisseur`, `date`, `qte`, `description`) VALUES
+(2, 'jusOrange.jpg', 'jus d\'orange', 0, 1, 1, '2022-05-12', 30, ''),
+(3, 'raisins.jpg', 'raisins', 0, 2, 1, '2022-11-22', 10, ''),
+(9, 'pomme.jpg', 'pommes', 3000, 2, 1, '2022-12-10', 50, ''),
+(4, 'jusRaisin.jpg', 'jus de raisins', 1500, 1, 1, '2022-11-28', 40, ''),
+(6, 'mangue.jpg', 'Mangue', 3000, 2, 1, '2022-11-12', 10, ''),
+(7, 'orange.jpg', 'Oranges', 3000, 2, 1, '2022-11-28', 50, ''),
+(8, 'pasteque.jpg', 'Pasteque', 3000, 2, 1, '2022-02-13', 30, ''),
+(10, 'briqueJus.jpg', 'jus cocktail', 4000, 1, 1, '2022-12-14', 45, ''),
+(11, 'goyave.jpg', 'goyaves', 1500, 2, 1, '2022-11-21', 100, '');
 
 -- --------------------------------------------------------
 
@@ -280,21 +326,22 @@ INSERT INTO `produits` (`id`, `image`, `nom`, `prix`, `cathegorie`, `fournisseur
 DROP TABLE IF EXISTS `quartiers`;
 CREATE TABLE IF NOT EXISTS `quartiers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
+  `nomQuartier` varchar(255) NOT NULL,
   `etat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `quartiers`
 --
 
-INSERT INTO `quartiers` (`id`, `nom`, `etat`) VALUES
+INSERT INTO `quartiers` (`id`, `nomQuartier`, `etat`) VALUES
 (1, 'emana', 0),
 (2, 'nlongkak', 0),
 (3, 'ngouso', 0),
 (4, 'etoudi', 0),
-(5, 'manguier', 0);
+(5, 'manguier', 0),
+(6, 'bonamoussadi', 0);
 
 -- --------------------------------------------------------
 
@@ -305,19 +352,20 @@ INSERT INTO `quartiers` (`id`, `nom`, `etat`) VALUES
 DROP TABLE IF EXISTS `villes`;
 CREATE TABLE IF NOT EXISTS `villes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
+  `nomVille` varchar(255) NOT NULL,
   `etat` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `villes`
 --
 
-INSERT INTO `villes` (`id`, `nom`, `etat`) VALUES
+INSERT INTO `villes` (`id`, `nomVille`, `etat`) VALUES
 (1, 'douala', 0),
 (2, 'soa', 0),
-(3, 'yaounde', 0);
+(3, 'yaounde', 0),
+(4, 'bafoussam', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
